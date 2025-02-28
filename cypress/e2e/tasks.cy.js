4ca7a19301fe6ad41152c1e41fc92eb99d79f6b1
Joao Lopes <jgsantanalopes@gmail.com>
2025-02-28 16:02:21 -0300

    add first testcase

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,1 +1,10 @@
 /// <reference types="cypress" />
+
+describe('tarefas', () => {
+    it('Deve cadastrar uma nova tarefa', () => {
+        cy.visit('http://127.0.0.1:3000')
+
+        cy.get('input[placeholder="Add a new Task"]')
+          .type('Ler um livro de node.js')
+    })
+})
