a88945417d7fb96129e65b48356feea42edd871b
Joao Lopes <jgsantanalopes@gmail.com>
2025-03-06 16:50:26 -0300

    add new flow for delete tasks before run test

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,10 +1,24 @@
 /// <reference types="cypress" />
 
 describe('tarefas', () => {
-    it('Deve cadastrar uma nova tarefa', () => {
-        cy.visit('http://127.0.0.1:3000')
-
-        cy.get('input[placeholder="Add a new Task"]')
-          .type('Ler um livro de node.js')
+  it('Deve cadastrar uma nova tarefa', () => {
+    cy.request({
+      url: 'http://localhost:3333/helper/tasks',
+      method: 'DELETE',
+      body: { name: 'Ler um livro de node.js' }
+    }).then(response => {
+      expect(response.status).to.eq(204)
     })
+
+    cy.visit('http://127.0.0.1:3000')
+
+    cy.get('input[placeholder="Add a new Task"]')
+      .type('Ler um livro de node.js')
+
+    cy.contains('button', 'Create')
+      .click()
+
+    cy.contains('main div p', 'Ler um livro de node.js')
+      .should('be.visible')
+  })
 })
